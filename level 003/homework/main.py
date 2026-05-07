9b1e942e98c229a717b9a6aa0b701eb275360a6e
salogiorgadze <salomegiorgadzete05@geolab.edu.ge>
2026-05-07 15:38:15 +0400

    commit

---
+++ level 003/homework/main.py
@@ -0,0 +1,32 @@
+# 3) მომხმარებელმა შეიყვანოს სახელი და დაბეჭდე.
+name = input('enter your name: ')
+print(name)
+# 4) მომხმარებელმა შეიყვანოს რიცხვი და დაბეჭდე.
+num = int(input("enter any number: "))
+print(num)
+# 5) მომხამრებლმა შეიყვანოს სახელი და დაუმატე 'hello'
+username = input("enter your name: ")
+print(f"hello {username}")
+# 6) შეიყვანე რიცხვი და დაუმატე 1.
+num1 = int(input("enter any number: "))
+print(num1 + 1)
+# 7) შეიყვანე 2 რიცხვი და დაბეჭდე ჯამი.
+number1 = int(input("enter any number: "))
+number2 = int(input("enter any number: "))
+print(number1 + number2)
+# 8) შეიყვანე ათწილადი რიცხვი და დაბეჭდე.
+number3 = float(input("enter any number: "))
+print(number3)
+# 9) შეიყვანე რიცხვი და გაამრავლე 2-ზე.
+number4 = int(input("enter any number: "))
+print(number4 * 2)
+# 10) მომხმარებელს შემოაყვანინე ინფორმაცია და მოახდინე ყველა ნასწავლ მათემატიკურ ოპერატორებზე მათემატიკური ოპერაციები(+ ; - ; * ; ** ; / ; // ; %)
+num5 = int(input("enter any number: "))
+num6 = int(input("enter any number: "))
+print(num5 + num6)
+print(num5 - num6)
+print(num5 * num6)
+print(num5 / num6)
+print(num5 // num6)
+print(num5 % num6)
+print(num5 * num6)
